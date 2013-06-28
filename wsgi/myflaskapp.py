e7015400fdbedb0bdb792b56f401ff48a8ca9d83
Gordon Haff <bitmason@gmail.com>
2013-06-28 16:57:48 -0400

    added within. Switched lat/long

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -7,6 +7,8 @@ from bson import json_util
 from bson import objectid
 import re
 
+# Note order of coordinates is long/lat
+
 app = Flask(__name__)
 #add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
@@ -39,6 +41,25 @@ def near():
 
     #use the request parameters in the query
     result = db.gaugepoints.find({"pos" : { "$near" : [lon,lat]}})
+
+    #turn the results into valid JSON
+    return str(json.dumps({'results' : list(result)},default=json_util.default))
+
+#find gauges within a lot/long bounding box passed in as query parameters (within?lat1=45.5&&lon1=-82&lat2=42&lon2=-84)
+@app.route("/ws/gauges/within")
+def near():
+    #setup the connection
+    conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+    db = conn.gauges
+
+    #get the request parameters
+    lat1 = float(request.args.get('lat1'))
+    lon1 = float(request.args.get('lon1'))
+    lat2 = float(request.args.get('lat2'))
+    lon2 = float(request.args.get('lon2'))
+
+    #use the request parameters in the query
+    result = db.gaugepoints.find(("pos": {"$within": {"$box" : [[lon1,lat1],[lon2,lat2]]}}})
 
     #turn the results into valid JSON
     return str(json.dumps({'results' : list(result)},default=json_util.default))
