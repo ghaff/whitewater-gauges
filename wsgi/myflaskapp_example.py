56cdc9dc4ef57cd0dea85504ad32fa8b7b0c2f47
Gordon Haff <bitmason@gmail.com>
2013-06-28 16:13:22 -0400

    switched to gauge db

--- wsgi/myflaskapp_example.py
+++ wsgi/myflaskapp_example.py
@@ -11,45 +11,58 @@ app = Flask(__name__)
 #add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
-#a base urls that returns all the gauges in the collection (of course in the future we would implement paging)
-@app.route("/ws/gauges")
-def gauges():
-    #setup the connection to the gauges database
+#a base urls that returns all the parks in the collection (of course in the future we would implement paging)
+@app.route("/ws/parks")
+def parks():
+    #setup the connection to the parks database
     conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    db = conn.gauges
+    db = conn.parks
 
-    #query the DB for all the gaugepoints
-    result = db.gaugepoints.find()
+    #query the DB for all the parkpoints
+    result = db.parkpoints.find()
 
     #Now turn the results into valid JSON
     return str(json.dumps({'results':list(result)},default=json_util.default))
 
 
+#return a specific park given it's mongo _id
+@app.route("/ws/parks/park/<parkId>")
+def onePark(parkId):
+    #setup the connection
+    conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
+    db = conn.parks
 
-#find gauges near a lat and long passed in as query parameters (near?lat=45.5&lon=-82)
-@app.route("/ws/gauges/near")
+    #query based on the objectid
+    result = db.parkpoints.find({'_id': objectid.ObjectId(parkId)})
+
+    #turn the results into valid JSON
+    return str(json.dumps({'results' : list(result)},default=json_util.default))
+
+
+#find parks near a lat and long passed in as query parameters (near?lat=45.5&lon=-82)
+@app.route("/ws/parks/near")
 def near():
     #setup the connection
     conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    db = conn.gauges
+    db = conn.parks
 
     #get the request parameters
     lat = float(request.args.get('lat'))
     lon = float(request.args.get('lon'))
 
     #use the request parameters in the query
-    result = db.gaugepoints.find({"pos" : { "$near" : [lon,lat]}})
+    result = db.parkpoints.find({"pos" : { "$near" : [lon,lat]}})
 
     #turn the results into valid JSON
     return str(json.dumps({'results' : list(result)},default=json_util.default))
 
 
-#find gauges with a certain name (use regex) near a lat long pair such as above
-@app.route("/ws/gauges/name/near/<name>")
+#find parks with a certain name (use regex) near a lat long pair such as above
+@app.route("/ws/parks/name/near/<name>")
 def nameNear(name):
     #setup the connection
     conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
-    db = conn.gauges
+    db = conn.parks
 
     #get the request parameters
     lat = float(request.args.get('lat'))
@@ -59,7 +72,7 @@ def nameNear(name):
     myregex = re.compile(name, re.I)
 
     #use the request parameters in the query along with the regex
-    result = db.gaugepoints.find({"Name" : myregex, "pos" : { "$near" : [lon,lat]}})
+    result = db.parkpoints.find({"Name" : myregex, "pos" : { "$near" : [lon,lat]}})
 
     #turn the results into valid JSON
     return str(json.dumps({'results' : list(result)},default=json_util.default))
