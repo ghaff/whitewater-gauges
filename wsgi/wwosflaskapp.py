d9a5d667155cbb6defc47dbb8705a68f5dae38e0
Gordon Haff <bitmason@gmail.com>
2013-07-20 14:52:45 -0400

    try backing up

--- wsgi/wwosflaskapp.py
+++ wsgi/wwosflaskapp.py
@@ -66,7 +66,7 @@ def update():
 # USGS requires a major filter. I'm using huc (hydrological area)
 # Range from 01 to 21
 
-    for i in range(1,22):
+    for i in range(1,2):
     
         if i < 10:
             hucstring = "0" + str(i)
@@ -75,62 +75,62 @@ def update():
             
         returnstring = "All OK"
         
-        try:
+
 
         
-            requesturl = "http://waterservices.usgs.gov/nwis/iv/?format=json,1.1&huc="+ hucstring + "&parameterCd=00060,00065&siteType=ST"
+        requesturl = "http://waterservices.usgs.gov/nwis/iv/?format=json,1.1&huc="+ hucstring + "&parameterCd=00060,00065&siteType=ST"
 
-            req = urllib2.Request(requesturl)
-            opener = urllib2.build_opener()
-            f = opener.open(req)
-            entry = json.loads(f.read()) 
+        req = urllib2.Request(requesturl)
+        opener = urllib2.build_opener()
+        f = opener.open(req)
+        entry = json.loads(f.read()) 
     
 
-            count = int (len(entry['value']['timeSeries']) - 1)
+        count = int (len(entry['value']['timeSeries']) - 1)
 
-            while count >= 0:
+        while count >= 0:
 #We construct an array of the relevant values associated with a guage number
 #Note that gage height and discharge are in separate entries
 #Right here we're just filling out the "permanent" values
 
 
 #Gauge Number. This will be the dictionary index
-                agaugenum = entry['value']['timeSeries'][count]['sourceInfo']['siteCode'][0]['value']
+            agaugenum = entry['value']['timeSeries'][count]['sourceInfo']['siteCode'][0]['value']
 
 #Site Name
 #Going to assume that all the "permanent" attributes of a guage number are the
 #same across entries.
 
 # save the variable code
-                variablecode = str(entry['value']['timeSeries'][count]['variable']['variableCode'][0]['variableID'])
+            variablecode = str(entry['value']['timeSeries'][count]['variable']['variableCode'][0]['variableID'])
 
 # save the variable value
 
-                variablevalue = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['value'])
+            variablevalue = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['value'])
 
 # save the time stamp
 
-                creationtime  = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['dateTime'])
+            creationtime  = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['dateTime'])
 
 #Gage ht. ft. variableID 45807202
 
-                if variablecode == '45807202':
-                    db.gaugepoints.update({"_id":agaugenum},{"$set":{"height":variablevalue}})
+            if variablecode == '45807202':
+                db.gaugepoints.update({"_id":agaugenum},{"$set":{"height":variablevalue}})
 
 
 #Discharge cfs variableID 45807197
 
-                if variablecode == '45807197':
-                    db.gaugepoints.update({"_id":agaugenum},{"$set":{"flow":variablevalue}})
+            if variablecode == '45807197':
+                db.gaugepoints.update({"_id":agaugenum},{"$set":{"flow":variablevalue}})
          
 #save creation time so that we can throw out any stale data
     
-                db.gaugepoints.update({"_id":agaugenum},{"$set":{"timestamp":creationtime}})
+            db.gaugepoints.update({"_id":agaugenum},{"$set":{"timestamp":creationtime}})
    
-                count = count - 1
+            count = count - 1
 #                returnstring = returnstring + hucstring + "OK\r"
-            except:
-                pass
+#            except:
+#                pass
 # throttle the calls
             
 #            sleep(30)
