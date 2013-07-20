74e99a1365eccb99ca385788b4ca06fb2d2a67f6
Gordon Haff <bitmason@gmail.com>
2013-07-20 18:32:19 -0400

    redid flask update code

--- wsgi/wwosflaskapp.py
+++ wsgi/wwosflaskapp.py
@@ -58,91 +58,81 @@ def within():
 # for manual updates
 @app.route("/ws/gauges/update")
 def update():
+
+    statelist = ["al","ak","az","ar","ca","co","ct","de","dc","fl","ga","hi","id","il","in","ia","ks","ky","la","me","md","ma","mi","mn","ms","mo","mt","ne","nv","nh","nj","nm","ny","nc","nd","oh","ok","or","pa","ri","sc","sd","tn","tx","ut","vt","va","wa","wv","wi","wy","pr"]
+    
     #setup the connection to the gauges database
     conn = pymongo.Connection(os.environ['OPENSHIFT_MONGODB_DB_URL'])
     db = conn.gauges
         
         
-# USGS requires a major filter. I'm using huc (hydrological area)
-# Range from 01 to 21
-
-#This does not seem to complete reliably
-
-    for i in range(1,22):
+    # USGS requires a major filter. I'm using state name
+    # Note that some filters seem to produce JSON rsponses that are too large to process
     
-        if i < 10:
-            hucstring = "0" + str(i)
-        else:
-            hucstring = str(i)
-            
-        returnstring = "Update completed"
-        
-
-
-        
-        requesturl = "http://waterservices.usgs.gov/nwis/iv/?format=json,1.1&huc="+ hucstring + "&parameterCd=00060,00065&siteType=ST"
-
-        try:
+    for i in statelist:
+    
+        requesturl = "http://waterservices.usgs.gov/nwis/iv/?format=json,1.1&stateCd=" + i +"&parameterCd=00060,00065&siteType=ST"
+    
+    
             #code
-            req = urllib2.Request(requesturl)
-            opener = urllib2.build_opener()
-            f = opener.open(req)
-        except:
-            continue
+        req = urllib2.Request(requesturl)
+        opener = urllib2.build_opener()
+        f = opener.open(req)
+    
         
         entry = json.loads(f.read())
         
-#        returnstring += hucstring + "OK "
     
-
         count = int (len(entry['value']['timeSeries']) - 1)
-
+    
         while count >= 0:
-#We construct an array of the relevant values associated with a guage number
-#Note that gage height and discharge are in separate entries
-#Right here we're just filling out the "permanent" values
-
-
-#Gauge Number. This will be the dictionary index
+    #We construct an array of the relevant values associated with a guage number
+    #Note that gage height and discharge are in separate entries
+    #Right here we're just filling out the "permanent" values
+    
+    
+    #Gauge Number. This will be the dictionary index
             agaugenum = entry['value']['timeSeries'][count]['sourceInfo']['siteCode'][0]['value']
-
-#Site Name
-#Going to assume that all the "permanent" attributes of a guage number are the
-#same across entries.
-
-# save the variable code
+    
+    #Site Name
+    #Going to assume that all the "permanent" attributes of a guage number are the
+    #same across entries.
+    
+    # save the variable code
             variablecode = str(entry['value']['timeSeries'][count]['variable']['variableCode'][0]['variableID'])
-
-# save the variable value
-
-            variablevalue = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['value'])
-
-# save the time stamp
-
-            creationtime  = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['dateTime'])
-
-#Gage ht. ft. variableID 45807202
-
+    
+    # save the variable value
+            try:
+                variablevalue = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['value'])
+            except:
+                variablevalue = ""
+    
+    # save the time stamp
+    
+            try:
+                creationtime  = str(entry['value']['timeSeries'][count]['values'][0]['value'][0]['dateTime'])
+            except:
+                creationtime = ""
+    
+    #Gage ht. ft. variableID 45807202
+    
             if variablecode == '45807202':
                 db.gaugepoints.update({"_id":agaugenum},{"$set":{"height":variablevalue}})
-
-
-#Discharge cfs variableID 45807197
-
+    
+    
+    #Discharge cfs variableID 45807197
+    
             if variablecode == '45807197':
                 db.gaugepoints.update({"_id":agaugenum},{"$set":{"flow":variablevalue}})
          
-#save creation time so that we can throw out any stale data
+    #save creation time so that we can throw out any stale data
     
             db.gaugepoints.update({"_id":agaugenum},{"$set":{"timestamp":creationtime}})
-   
+    
             count = count - 1
-
             
-            sleep(30)
-
-    return returnstring     
-        
+    return "Update completed. "
+            
 
 
 @app.route("/")
